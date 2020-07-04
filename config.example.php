0f6b4350f5283fefcb5e069f6a8231d9db56e387
oleh-kuklenko <oleg.kklimenok@gmail.com>
2020-07-04 21:45:21 +0300

    Add config example and robots.txt

---
+++ config.example.php
@@ -0,0 +1,20 @@
+<?php
+// Base
+define('DIR_APPLICATION', '');
+define('DIR_CLASS', DIR_APPLICATION . '/');
+define('DIR_LIBRARY', DIR_APPLICATION . '/');
+define('DIR_LOGS', DIR_APPLICATION . '/');
+define('__URL__', []);
+
+// Telegram
+define('__TOKEN__', '');
+define('__CHAT_ID__', 0);
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', '');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
